297442949cf590c06bee6ffd37829986f2973abb
sunub <bsc5672@gmail.com>
2022-12-01 15:33:00 +0900

    chore : changing complier webpack to vite
    Change the complier because vite is faster and more useful.

--- src/base/Utils/Debug.js
+++
@@ -1,7 +0,0 @@
-import * as dat from "lil-gui";
-
-export default class Debug {
-  constructor() {
-    this.ui = new dat.GUI();
-  }
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./style.css";
-import App from "./base/app.js";
-
-const app = new App();
